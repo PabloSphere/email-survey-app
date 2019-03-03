189418bc4183a2dbb19cf31eb492af17ef2f32dd
Alan Thompson <alanthompson.se@gmail.com>
2019-03-03 06:32:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const key = require('./config/key')
+require('./models/User')
+const passportConfig = require('./services/passport');
+
+mongoose.connect(key.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-res.send({hi: 'there'});
-});
+authRoutes(app);
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT)
+
+/*
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/admin";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+ // perform actions on the collection object
+  client.close();
+});
+*/
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+
+const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy
+// const keys = require('./config/key')
+
+module.exports = (app) => {
+  app.get('/auth/google', passport.authenticate('google', {
+    scope:['profile', 'email']
+  })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'))
+}
